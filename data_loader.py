eff7d85383945e19a6aece16b073537dc9e403b3
Kenneth Cordero <kencordero@gmail.com>
2019-06-14 11:45:57 -0500

    refactor

--- data_loader.py
+++ data_loader.py
@@ -1,45 +1,42 @@
 import json
 from random import shuffle
 
+
 def load_pieces():
-    pcsFile = 'data/pieces.json'
-    with open(pcsFile) as f:
+    filename = 'data/pieces.json'
+    with open(filename) as f:
         data = json.load(f)
     pieces = data['pieces']
     shuffle(pieces)
     return pieces
 
+
 def load_spaces():
-    spcFile = 'data/spaces.json'
-    with open(spcFile) as f:
+    filename = 'data/spaces.json'
+    with open(filename) as f:
         data = json.load(f)
     return data['spaces']
 
-def load_CommunityChest():
-    CCfile = "data/community_chest.json"
-    with open(CCfile) as f:
-        data = json.load(f)
-    CC = data['cards']
-    shuffle(CC)
-    return CC
 
-def load_Chance():
-  chanceFile = "data/chance.json"
-  with open(chanceFile) as f:
-      data = json.load(f)
-  C = data['cards']
-  shuffle(C)
-  return C
+def load_deck(deck_type):
+    deck_file = 'data/{}.json'.format(deck_type)
+    with open(deck_file) as f:
+        data = json.load(f)
+    deck = data['cards']
+    shuffle(deck)
+    return deck
+
 
 def load_properties():
-    propFile = "data/properties.json"
-    with open(propFile) as f:
+    filename = 'data/properties.json'
+    with open(filename) as f:
         data = json.load(f)
     return data['properties']
+
 
 def load_data():
     pieces = load_pieces()
     spaces = load_spaces()
-    CommunityChest = load_CommunityChest()
-    Chance = load_Chance()
+    CommunityChest = load_deck('community_chest')
+    Chance = load_deck('chance')
     properties = load_properties()
--- models/game.py
+++ models/game.py
@@ -1,10 +1,9 @@
-from ..data_loader import (load_pieces, load_spaces, load_CommunityChest, load_Chance, load_properties)
+from ..data_loader import (load_pieces, load_spaces, load_deck, load_properties)
+
 
 class Game(object):
     def __init__(self, x):
         self.pieces = load_pieces()
         self.spaces = load_spaces()
-        self.cards = {}
-        self.cards['CommunityChest'] = load_CommunityChest()
-        self.cards['Chance'] = load_Chance()
+        self.cards = {'CommunityChest': load_deck('community_chest'), 'Chance': load_deck('chance')}
         self.properties = load_properties()
